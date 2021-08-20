437257d155b897d0849b51cb499d9cec84f63f63
alzahrawais <magaries1728@gmail.com>
2021-08-20 11:24:43 +0500

    updated random

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router =express.Router();
+const {check,validationResult}=require('express-validator/check')
 
-router.get('/',(req,res)=>res.send('User API'))
+router.post('/',[
+    check('name','Please enter the Name').isEmpty().not(),
+    check('email','Please Enter a valid Email').isEmail(),
+    check('password',"Enter a password of length 6 or greater").isLength({min:6})
+],
+(req,res)=>{
+   const errors = validationResult(req);
+       if(!errors.isEmpty){
+           res.status(400).json({errors: errors.array()})
+       }
+       console.log(req.body);
+       res.send('User API');
+})
 
 module.exports=router;
